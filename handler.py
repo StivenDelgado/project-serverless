2f2e0a187fbb74fcc499b725e4329a5e7d9dc20b
Stiven Delgado <delgadostiven123@gmail.com>
2025-02-23 12:35:48 -0500

    project done

---
+++ handler.py
@@ -0,0 +1,21 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def saludo(event, context):
+    body = {
+        "message": "Hola mundo"
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
